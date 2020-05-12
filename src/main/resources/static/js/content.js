618fcac601f0e050cbcbceaa0ac23cfacac1edd6
xuyuanfeng <986771570@qq.com>
2020-05-13 00:03:24 +0800

    [future] front template add

--- src/main/resources/static/js/content.js
+++ src/main/resources/static/js/content.js
@@ -38,7 +38,9 @@ $('.close-link').click(function () {
 //判断当前页面是否在iframe中
 if (top == this) {
     var gohome = '<div class="gohome"><a class="animated bounceInUp" href="/" title="返回首页"><i class="fa fa-home"></i></a></div>';
+
     $('body').append(gohome);
+
 }
 
 //animation.css
